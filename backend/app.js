d1e3a9f1f43d7c6cc4278d0c1c7d05585cb60a72
owlsspell <arhipowael@gmail.com>
2023-07-21 17:24:20 +0300

    add cors config

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,12 @@ import { sql } from '@vercel/postgres';
 import path from "path";
 import cors from "cors";
 
-app.use(cors())
+const corsOptions = {
+  origin: 'https://employee-dashboard-ecru.vercel.app/',        //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
