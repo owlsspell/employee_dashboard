ecbd64e7af32c6895033fd6526439b3baeaf8fe6
owlsspell <arhipowael@gmail.com>
2023-07-24 16:02:06 +0300

    add cors for local

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,15 +1,7 @@
 import express from "express";
 const app = express();
 const port = process.env.PORT || 5000;
-// import cors from "cors";
 import employees from "./employees.js"
-
-// const corsOptions = {
-//   origin: '*',
-//   optionSuccessStatus: 200,
-// }
-
-// app.use(cors(corsOptions))
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
