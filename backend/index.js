fb6ce36d36f6565af1386e7716ca285150d3d869
owlsspell <arhipowael@gmail.com>
2023-07-24 14:46:33 +0300

    add headers vercel

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,18 @@
 import express from "express";
 const app = express();
 const port = process.env.PORT || 5000;
-import cors from "cors";
+// import cors from "cors";
 import employees from "./api/employees.js"
 
-const corsOptions = {
-  origin: '*',
-  optionSuccessStatus: 200,
-}
-// app.use(history({
-//   // index: '/index.js',
-//   logger: console.log.bind(console)
-// }));
-// app.use(history({
-//   index: '/index.js'
-// }));
+// const corsOptions = {
+//   origin: '*',
+//   optionSuccessStatus: 200,
+// }
+
+// app.use(cors(corsOptions))
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-app.use(cors(corsOptions))
 
 app.use('/api/', employees)
 
