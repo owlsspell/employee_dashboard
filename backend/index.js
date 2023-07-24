9c6285d83c87760498a5241779e75878b8d9c4c5
owlsspell <arhipowael@gmail.com>
2023-07-24 15:30:16 +0300

    delete cors

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
 import express from "express";
 const app = express();
 const port = process.env.PORT || 5000;
-import cors from "cors";
+// import cors from "cors";
 import employees from "./api/employees.js"
 
-const corsOptions = {
-  origin: '*',
-  optionSuccessStatus: 200,
-}
+// const corsOptions = {
+//   origin: '*',
+//   optionSuccessStatus: 200,
+// }
 
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
