cc85584453c1adcefb181801143edd23482a4e4c
owlsspell <arhipowael@gmail.com>
2023-08-04 18:16:39 +0300

    remove check scope

--- backend/api/main.js
+++ backend/api/main.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 import employees from "../src/routes/employees.js"
 import charts from "../src/routes/charts.js"
-import { checkJwt, checkScopes } from "../src/middlewares/auth.js";
+import { checkJwt } from "../src/middlewares/auth.js";
 
 router.get('/', (req, res) => {
     res.send("It is working!")
