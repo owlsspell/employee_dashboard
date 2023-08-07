fa07f292141a0a62a3ff053c5cb7538a71c466b1
owlsspell <arhipowael@gmail.com>
2023-08-07 16:33:05 +0300

    add toogler mode

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -1,8 +1,9 @@
 
 import { auth, requiredScopes } from 'express-oauth2-jwt-bearer'
-
+console.log(process.env.MODE);
+console.log('111', process.env.MODE === "development" ? process.env.AUTH0_AUDIENSE_DEV : process.env.AUTH0_AUDIENSE_PROD);
 export const checkJwt = auth({
-    audience: process.env.PRODUCTION ? process.env.AUTH0_AUDIENSE_PROD : process.env.AUTH0_AUDIENSE_DEV,
+    audience: (process.env.MODE === "development" ? process.env.AUTH0_AUDIENSE_DEV : process.env.AUTH0_AUDIENSE_PROD),
     issuerBaseURL: process.env.AUTH0_BASE_URL,
     tokenSigningAlg: 'RS256'
 });
