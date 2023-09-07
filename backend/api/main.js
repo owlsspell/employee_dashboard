bad0b4ea0dcd10d43cab1a53deddabc6c845be34
owlsspell <arhipowael@gmail.com>
2023-09-07 16:47:23 +0300

    add allowCors

--- backend/api/main.js
+++ backend/api/main.js
@@ -4,6 +4,9 @@ const router = express.Router();
 import employees from "../src/routes/employees.js"
 import charts from "../src/routes/charts.js"
 import { checkJwt } from "../src/middlewares/auth.js";
+import allowCors from "../src/allowCors.js";
+
+router.use(allowCors)
 
 router.get('/', (req, res) => {
     res.send("It is working!")
